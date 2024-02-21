10cef165b8ddbd2c60e8b3431cef43143c292e58
Arjun Neervannan <arjun.neervannan@gmail.com>
2024-02-20 21:19:44 -0500

    smooth simulation

--- path_planning_v2/mechanics.py
+++ path_planning_v2/mechanics.py
@@ -2,4 +2,8 @@ import pyglet
 from pyglet import shapes
 from math import sin, cos
 
-
+def create_drones(N, batch):
+    drones = []
+    for i in range(N):
+        drone = shapes.Circle(x=0, y=0, radius=50, color=(50, 225, 30), batch=batch)
+        drones.append(drone)
